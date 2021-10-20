4fc575e3ea89502acc5b7948ec3d5fd0e8016098
lacizeidler <laci.zeidler@gmail.com>
2021-10-20 15:13:57 -0500

    final page

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,31 +1,36 @@
-import { getOrders } from "./database.js"
-import { Metals } from "./Metals.js"
-Metals()
+import { getMetals, getOrders, getSizes, getStyles } from "./database.js"
 
 const buildOrderListItem = (order) => {
+    const metals = getMetals()
+    const sizes = getSizes()
+    const styles = getStyles()
+
+    // Remember that the function you pass to find() must return true/false
+    const foundMetal = metals.find(
+        (metal) => {
+            return metal.id === order.metalId
+        }
+    )
+    const foundSizes= sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+    const foundStyles = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+    const totalCost = foundMetal.price + foundSizes.price + foundStyles.price
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
-    </li>`
+    Order #${order.id} cost ${costString}
+</li>`
 }
-
-// const metals = getMetals()
-
-// Remember that the function you pass to find() must return true/false
-// const foundMetal = metals.find(
-//     (metal) => {
-//         return metal.id === order.metalId
-//     }
-// )
-// const totalCost = foundMetal.price
-
-// const costString = totalCost.toLocaleString("en-US", {
-//     style: "currency",
-//     currency: "USD"
-// })
-
-// `<li>
-//     Order #${order.id} cost ${costString}
-// </li>`
 
 
 export const Orders = () => {
